99bc0525e008a5e6b627e24de89d07048d4fabf9
LuisLaptop <luispim2005@gmail.com>
2024-06-25 11:04:36 -0300

    Order, Instant, ISO 8601 p2

---
+++ src/main/java/com/awsudemy/course/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.awsudemy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.awsudemy.course.entities.Order;
+import com.awsudemy.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+	
+		return repository.findAll();
+	
+	}
+	
+	public Order findByID(long id) {	
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
