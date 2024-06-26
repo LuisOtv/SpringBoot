6397206dc80a562edfd7bfd5cd4182d48225bbc2
LuisLaptop <luispim2005@gmail.com>
2024-06-26 09:55:17 -0300

    Category

---
+++ src/main/java/com/awsudemy/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.awsudemy.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.awsudemy.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
