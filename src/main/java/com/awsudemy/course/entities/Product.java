5f88fa392a8ea7e9cb3efb3f602a98982d8ca2a3
LuisLaptop <luispim2005@gmail.com>
2024-06-26 10:27:06 -0300

    Product

--- src/main/java/com/awsudemy/course/entities/Product.java
+++ src/main/java/com/awsudemy/course/entities/Product.java
@@ -13,24 +13,30 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
+@Table(name = "tb_products")
+public class Product  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String desgription;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {}
-	
-	public Category(Long id,String name) {
+	public Product() {}
+
+	public Product(Long id, String name, String desgription, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.desgription = desgription;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -48,9 +54,33 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDesgription() {
+		return desgription;
+	}
+
+	public void setDesgription(String desgription) {
+		this.desgription = desgription;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -66,8 +96,10 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
+	
+	
+	
 }
