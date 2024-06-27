92daa342b1a138f91db44f9d12f11f7510dbabd5
LuisLaptop <luispim2005@gmail.com>
2024-06-27 09:44:49 -0300

    Many to many assosiation with join table

--- src/main/java/com/awsudemy/course/repositories/ProductRepository.java
+++ src/main/java/com/awsudemy/course/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.awsudemy.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.awsudemy.course.entities.Category;
 import com.awsudemy.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
